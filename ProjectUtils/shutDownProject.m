fa7e0d4aa77959f8f1e2bc9cfbda2d50e30ee1dd
sreeramgorthy <sreeramgorthy@gmail.com>
2020-12-06 19:15:25 +0530

    Designed simulink model. To find the optimal value for M2 and k12 so that M1 does not vibrate.

---
+++ ProjectUtils/shutDownProject.m
@@ -0,0 +1,4 @@
+% This script executes during closing of the project.
+clearvars
+clc
+bdclose all
---
+++ ProjectUtils/startUpProject.m
@@ -0,0 +1,4 @@
+% This script executes during the start of the project.
+clearvars
+clc
+bdclose all
